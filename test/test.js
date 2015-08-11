cbc52850066ed51c4e45bc2cbc54b412eba87c60
timkellogg <tim.kellogg@gmail.com>
2015-08-10 22:15:50 -0700

    get first test to pass; restructure task prototype

--- test/test.js
+++ test/test.js
@@ -2,15 +2,28 @@ var Task = require('../lib/task.js'),
     chai = require('chai'),
     expect = chai.expect;
 
-
-
 describe('Task', function () {
-
-
-  it('returns the description', function (done) {
-    var testTask = new Task("this is a task");
-    expect(testTask).to.have.property('description');
-    done();
+  describe('.all', function () {
+    it('should be empty at first', function (done) {
+      console.log(Task.tasks);
+      expect(Task.all()).to.equal([])
+      done();
+    });
   });
 
+  describe('#init', function () {
+    it('returns the description', function (done) {
+      var testTask = new Task();
+      testTask.init('this is a description');
+      expect(testTask).to.have.property('description');
+      done();
+    });
+
+    it('increments the collection of tasks', function (done) {
+      var testTask = new Task();
+      testTask.init("this is a task");
+      expect(testTask.tasks.length).to.equal([1]);
+      done();
+    });
+  });
 });
